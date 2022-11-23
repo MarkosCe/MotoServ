bc56b76fc7b3e10c19efafe45131c01a96fc3524
MarkosCe <cesar.markos99@gmail.com>
2022-11-23 02:35:43 -0600

    draw route for driver

--- app/src/main/java/com/example/motoserv/driver/MapDriverBookingActivity.java
+++ app/src/main/java/com/example/motoserv/driver/MapDriverBookingActivity.java
@@ -14,12 +14,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -27,9 +29,12 @@ import android.widget.Toast;
 
 import com.example.motoserv.R;
 import com.example.motoserv.providers.AuthProvider;
+import com.example.motoserv.providers.ClientBookingProvider;
 import com.example.motoserv.providers.ClientProvider;
 import com.example.motoserv.providers.GeofireProvider;
+import com.example.motoserv.providers.GoogleApiProvider;
 import com.example.motoserv.providers.TokenProvider;
+import com.example.motoserv.utils.DecodePoints;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -42,12 +47,24 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.SquareCap;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MapDriverBookingActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -63,8 +80,20 @@ public class MapDriverBookingActivity extends AppCompatActivity implements OnMap
     private GeofireProvider mGeofireProvider;
     private AuthProvider mAuthProvider;
     private ClientProvider mClientProvider;
+    private ClientBookingProvider mClientBookingProvider;
+
+    private LatLng mOriginlatlng;
+    private LatLng mDestinationLatlng;
+    private GoogleApiProvider mGoogleApiProvider;
+
+    private List<LatLng> mPolylineList;
+    private PolylineOptions mPolylineOptions;
 
     private TextView mTextViewClientBooking;
+    private TextView mTextViewOriginBooking;
+    private TextView mTextViewDestinationBooking;
+
+    private boolean mIsFirstTime = true;
 
     private String mExtraClientId;
 
@@ -79,11 +108,16 @@ public class MapDriverBookingActivity extends AppCompatActivity implements OnMap
         mGeofireProvider = new GeofireProvider("drivers_working");
         mAuthProvider = new AuthProvider();
         mClientProvider = new ClientProvider();
+        mClientBookingProvider = new ClientBookingProvider();
 
         mTextViewClientBooking = findViewById(R.id.text_view_name_client_booking);
+        mTextViewOriginBooking = findViewById(R.id.text_view_origin_client_booking);
+        mTextViewDestinationBooking = findViewById(R.id.text_view_destination_client_booking);
         mExtraClientId = getIntent().getStringExtra("idClient");
 
-        getClientBooking();
+        getClient();
+
+        mGoogleApiProvider = new GoogleApiProvider();
 
         // Get a handle to the fragment and register the callback.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -118,7 +152,13 @@ public class MapDriverBookingActivity extends AppCompatActivity implements OnMap
                                         .zoom(16f)
                                         .build()
                         ));
+
                         updateLocation();
+
+                        if (mIsFirstTime){
+                            mIsFirstTime = false;
+                            getClientBooking();
+                        }
                     }
                 }
             }
@@ -126,6 +166,39 @@ public class MapDriverBookingActivity extends AppCompatActivity implements OnMap
     }
 
     private void getClientBooking(){
+        mClientBookingProvider.getClientBooking(mExtraClientId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    String destination = String.valueOf(snapshot.child("destination"));
+                    String origin = String.valueOf(snapshot.child("origin"));
+                    double destinationLat = Double.parseDouble(snapshot.child("destinationLat").toString());
+                    double destinationLng = Double.parseDouble(snapshot.child("destinationLng").toString());
+
+                    double originLat = Double.parseDouble(snapshot.child("originLat").toString());
+                    double originLng = Double.parseDouble(snapshot.child("originLng").toString());
+
+                    mOriginlatlng = new LatLng(originLat, originLng);
+                    mDestinationLatlng = new LatLng(destinationLat, destinationLng);
+
+                    mTextViewOriginBooking.setText("Origen:" + origin);
+                    mTextViewDestinationBooking.setText("Destino" + destination);
+
+                    mMap.addMarker(new MarkerOptions().position(mOriginlatlng).title("AQUI").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_red)));
+
+                    //ruta del conductor hacia el usuario(cliente)
+                    drawRoute();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getClient(){
         mClientProvider.getClient(mExtraClientId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -137,6 +210,46 @@ public class MapDriverBookingActivity extends AppCompatActivity implements OnMap
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void drawRoute(){
+        mGoogleApiProvider.getDirections(mCurrentLocation, mOriginlatlng).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body());
+                    JSONArray jsonArray = jsonObject.getJSONArray("routes");
+                    JSONObject route = jsonArray.getJSONObject(0);
+                    JSONObject polylines = route.getJSONObject("overview_polyline");
+                    String points = polylines.getString("points");
+
+                    //mPolylineList = PolyUtil.decode(points);
+                    mPolylineList = DecodePoints.decodePoly(points);
+                    mPolylineOptions = new PolylineOptions();
+                    mPolylineOptions.color(Color.DKGRAY);
+                    mPolylineOptions.width(8f);
+                    mPolylineOptions.startCap(new SquareCap());
+                    mPolylineOptions.jointType(JointType.ROUND);
+                    mPolylineOptions.addAll(mPolylineList);
+                    mMap.addPolyline(mPolylineOptions);
+
+                    JSONArray legs = route.getJSONArray("legs");
+                    JSONObject leg = legs.getJSONObject(0);
+                    JSONObject distance = leg.getJSONObject("distance");
+                    JSONObject duration = leg.getJSONObject("duration");
+                    String distanceText = distance.getString("text");
+                    String durationText = duration.getString("text");
+
+                }catch (Exception e){
+                    Log.d("error", "Error encontrado " + e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
 
             }
         });
